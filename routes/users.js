aa8d243d08358eb1f93f270cd294f652f5e2559e
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-06 00:39:57 -0600

    set up the registration and upload file

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const uploads = multer({ dest: "./uploads" });
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
@@ -12,6 +14,41 @@ router.get("/register", (req, res, next) => {
 
 router.get("/login", (req, res, next) => {
   res.render("login", { title: "Login" });
+});
+
+router.post("/register", uploads.single("profileimage"), (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  if (req.file) {
+    console.log("Uploading file.....");
+    const profileimage = req.file.filename;
+  } else {
+    console.log(" No file uploaded ....");
+    const profileimage = "noimage.jpeg";
+  }
+  // Form Validator
+  req.checkBody("name", "Name field is required").notEmpty();
+  req.checkBody("email", "Email field is required").notEmpty();
+  req.checkBody("email", "Email is not valid").isEmail();
+  req.checkBody("username", "Username field is required").notEmpty();
+  req.checkBody("password", "Password field is required").notEmpty();
+  req
+    .checkBody("password2", "Passwords do not match")
+    .equals(req.body.password);
+  // Check Errors
+  const errors = req.validationErrors();
+
+  if (errors) {
+    res.render("register", {
+      errors: errors
+    });
+  } else {
+    console.log("No errors");
+  }
 });
 
 module.exports = router;
