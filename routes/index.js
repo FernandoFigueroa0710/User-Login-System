c9aa5fc2c68c9e8d4c7d5baa93ae3b3529734a4a
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-06 09:54:06 -0600

    switched to arrow function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const multer = require("multer");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index", { title: "Members" });
 });
 
